f78f7979d98801f8c6b1dcac903ab388c4063a44
bekazhumabekov106-cpu <bekazhumabekov106@gmail.com>
2026-02-08 12:49:05 +0500

    Update TextFooter.tsx

--- src/components/TextFooter.tsx
+++ src/components/TextFooter.tsx
@@ -11,17 +11,17 @@ export default function TextFooter() {
     <>
       {/* Left Text */}
       <h1
-        className={`absolute left-10 bottom-5 transform -translate-y-1/2 text-white text-4xl lg:text-5xl font-bold leading-tight ${playfairDisplay.className}`}
-      >
-        <span className="text-gray-400">Собери</span> <br /> все пары фотографий
-      </h1>
+  className={`absolute left-10 bottom-5 transform -translate-y-1/2 text-white text-4xl lg:text-5xl font-bold leading-tight ${playfairDisplay.className}`}
+>
+  <span className="text-gray-400">Собери</span> <br /> пары фото
+</h1>
 
-      {/* Right Text */}
-      <h1
-        className={`absolute right-10 bottom-5 transform -translate-y-1/2 text-white text-4xl lg:text-5xl font-bold leading-tight text-right ${playfairDisplay.className}`}
-      >
-        чтобы открыть <br /> <span className="text-gray-400">сюрприз для тебя 💝</span>
-      </h1>
+<h1
+  className={`absolute right-10 bottom-5 transform -translate-y-1/2 text-white text-4xl lg:text-5xl font-bold leading-tight text-right ${playfairDisplay.className}`}
+>
+  чтобы открыть <br /> <span className="text-gray-400">сюрприз 💝</span>
+</h1>
+
 
       {/* Copyright */}
       <p className="absolute bottom-5 right-5 text-white text-sm opacity-15 hover:opacity-50">
