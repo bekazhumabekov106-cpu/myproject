ddbed454ea0e047c2ce2c57e64534559d57000c5
bekazhumabekov106-cpu <bekazhumabekov106@gmail.com>
2026-02-08 13:05:19 +0500

    Update TextFooter.tsx

--- src/components/TextFooter.tsx
+++ src/components/TextFooter.tsx
@@ -11,11 +11,6 @@ export default function TextFooter() {
     <>
       {/* Left Text */}
       <h1
-  className={`absolute left-10 bottom-5 transform -translate-y-1/2 text-white text-3xl lg:text-4xl font-bold leading-tight ${playfairDisplay.className}`}
->
-  <span className="text-gray-400">Собери</span> <br /> все пары фото
-</h1>
-<h1
         className={`absolute left-10 bottom-5 transform -translate-y-1/2 text-white text-4xl lg:text-5xl font-bold leading-tight ${playfairDisplay.className}`}
       >
         <span className="text-gray-400">Собери</span> <br /> все пары фото
