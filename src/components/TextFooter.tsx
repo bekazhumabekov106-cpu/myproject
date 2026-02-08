686c0ab6634e5a48dfec298b57c11a573bd288e6
bekazhumabekov106-cpu <bekazhumabekov106@gmail.com>
2026-02-08 12:45:08 +0500

    Update TextFooter.tsx

--- src/components/TextFooter.tsx
+++ src/components/TextFooter.tsx
@@ -13,20 +13,20 @@ export default function TextFooter() {
       <h1
         className={`absolute left-10 bottom-5 transform -translate-y-1/2 text-white text-4xl lg:text-5xl font-bold leading-tight ${playfairDisplay.className}`}
       >
-        <span className="text-gray-400">Match</span> <br /> the photo pairs
+        <span className="text-gray-400">Собери</span> <br /> все пары фотографий
       </h1>
 
       {/* Right Text */}
       <h1
         className={`absolute right-10 bottom-5 transform -translate-y-1/2 text-white text-4xl lg:text-5xl font-bold leading-tight text-right ${playfairDisplay.className}`}
       >
-        to reveal <br /> <span className="text-gray-400">the surprise</span>
+        чтобы открыть <br /> <span className="text-gray-400">мой сюрприз для тебя 💝</span>
       </h1>
 
       {/* Copyright */}
       <p className="absolute bottom-5 right-5 text-white text-sm opacity-15 hover:opacity-50">
         © {new Date().getFullYear()}{" "}
-        <Link href="https://visibait.com">visibait.com</Link>
+        
       </p>
     </>
   );
