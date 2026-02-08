342eb258b995d6ad1394f302425d98b80822929a
bekazhumabekov106-cpu <bekazhumabekov106@gmail.com>
2026-02-08 12:47:06 +0500

    Update TextFooter.tsx

--- src/components/TextFooter.tsx
+++ src/components/TextFooter.tsx
@@ -20,7 +20,7 @@ export default function TextFooter() {
       <h1
         className={`absolute right-10 bottom-5 transform -translate-y-1/2 text-white text-4xl lg:text-5xl font-bold leading-tight text-right ${playfairDisplay.className}`}
       >
-        чтобы открыть <br /> <span className="text-gray-400">мой сюрприз для тебя 💝</span>
+        чтобы открыть <br /> <span className="text-gray-400">сюрприз для тебя 💝</span>
       </h1>
 
       {/* Copyright */}
